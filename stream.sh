75d7685334d315f89173c3560607d8e963a4b7ee
manilawestern-debug <manilawestern@gmail.com>
2026-04-08 07:24:44 +0800

    Update RTMP URL for Facebook live stream

--- stream.sh
+++ stream.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-ffmpeg -re -stream_loop -1 -i video.mp4 -c:v libx264 -preset veryfast -pix_fmt yuv420p -f flv rtmps://live-api-s.facebook.com:443/rtmp/FB-122125563339142480-0-Ab4QsPWxODfMVWtt47duTgy3
+ffmpeg -re -stream_loop -1 -i video.mp4 -c:v libx264 -preset veryfast -pix_fmt yuv420p -f flv rtmps://live-api-s.facebook.com:443/rtmp/FB-122125607511142480-0-Ab4GG6n1WrurjXG46D6P_5ST
 
 sleep 5
 done
