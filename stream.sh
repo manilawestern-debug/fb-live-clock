dd4b27fe94f75d47deb15c3a6f6f76a966268c8b
manilawestern-debug <manilawestern@gmail.com>
2026-04-08 07:06:32 +0800

    Change stream URL in stream.sh
    Updated the stream URL to a specific Facebook stream key.

--- stream.sh
+++ stream.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-ffmpeg -re -stream_loop -1 -i video.mp4 -c:v libx264 -preset veryfast -pix_fmt yuv420p -f flv rtmps://live-api-s.facebook.com:443/rtmp/YOUR_STREAM_KEY
+ffmpeg -re -stream_loop -1 -i video.mp4 -c:v libx264 -preset veryfast -pix_fmt yuv420p -f flv rtmps://live-api-s.facebook.com:443/rtmp/FB-122125563339142480-0-Ab4QsPWxODfMVWtt47duTgy3
 
 sleep 5
 done
